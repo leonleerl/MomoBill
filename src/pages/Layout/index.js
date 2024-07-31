a964eea1aab92e14e283ea9f42aede9b9219037e
leonleerl <leon.runlongli@gmail.com>
2024-07-31 15:25:19 +0800

    主题色定制化

--- src/pages/Layout/index.js
+++ src/pages/Layout/index.js
@@ -1,3 +1,4 @@
+import { Button } from "antd-mobile";
 import { Outlet } from "react-router-dom";
 
 const Layout = () => {
@@ -5,6 +6,10 @@ const Layout = () => {
     <>
       <Outlet />
       <div>我是layout</div>
+      <Button color="primary">测试全局</Button>
+      <div className="purple">
+        <Button color="danger">测试局部</Button>
+      </div>
     </>
   );
 };
