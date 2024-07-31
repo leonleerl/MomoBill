80eee45ad264ed91e3967d106d681b157d696667
leonleerl <leon.runlongli@gmail.com>
2024-07-31 14:59:26 +0800

    路由配置

--- src/index.js
+++ src/index.js
@@ -1,11 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import App from "./App";
-import sum from "@/test";
-
-const total = sum(1, 4);
-console.log(total);
+import { RouterProvider } from "react-router-dom";
+import router from "./router";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<App />);
+root.render(<RouterProvider router={router}></RouterProvider>);
