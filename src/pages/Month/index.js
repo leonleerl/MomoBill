7f96eba9bfe10a2f57c8e7c64a8169d7373bf951
leonleerl <leon.runlongli@gmail.com>
2024-07-31 17:54:45 +0800

    月度账单-按月分组实现

--- src/pages/Month/index.js
+++ src/pages/Month/index.js
@@ -1,10 +1,19 @@
 import { NavBar, DatePicker } from "antd-mobile";
 import "./index.scss";
-import { useState } from "react";
+import { useMemo, useState } from "react";
 import classNames from "classnames";
 import dayjs from "dayjs";
+import { useSelector } from "react-redux";
+import _ from "lodash";
 
 const Month = () => {
+  // 按月做数据分组
+  const billList = useSelector((state) => state.bill.billList);
+  const monthGroup = useMemo(() => {
+    // return出去计算之后的值
+    return _.groupBy(billList, (item) => dayjs(item.date).format("YYYY-MM"));
+  }, [billList]);
+  console.log(monthGroup);
   // 控制弹框的打开和关闭
   const [dateVisible, setDateVisible] = useState(false);
   //控制时间显示
